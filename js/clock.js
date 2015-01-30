1a1575c9768b13fbf35c5ef95b9099d2209f68e0
Miles Young <miles.young0@gmail.com>
2015-01-29 22:05:19 -0800

    Lab 4
    added colors and used forecast.io api

--- js/clock.js
+++ js/clock.js
@@ -12,3 +12,26 @@ var zero_pad = function(time) {
       return time;
    }
 }
+
+var getTemp = function() {
+	$.getJSON("https://api.forecast.io/forecast/3d85f330198ebc40d2d89489e16fa3b4/35.300399,-120.662362?callback=?", function (data) {
+      $("#forecastLabel").html(data.daily.summary);
+      $("#forecastIcon").attr("src", "img/" + data.daily.icon + ".png");
+      console.log(data.daily.icon);
+      var maxTemp = data.daily.data[0].temperatureMax;
+
+      if (maxTemp >= 90) {
+         $("body").addClass("hot");
+      } else if (maxTemp >= 80) {
+         $("body").addClass("warm");
+      } else if (maxTemp >= 70) {
+         $("body").addClass("nice");
+      } else if (maxTemp >= 60) {
+         $("body").addClass("chilly");
+      } else if (maxTemp < 60) {
+         $("body").addClass("cold");
+      }
+      console.log(data.daily.data);
+      console.log(data.daily.data[0].temperatureMax);
+   })
+}
